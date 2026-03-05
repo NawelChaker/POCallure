364818159334a5e63f19b1894771a730f2d530a3
NawelChaker <64616140+NawelChaker@users.noreply.github.com>
2026-03-05 12:11:03 +0100

    Create test_3.py

---
+++ tests/test_3.py
@@ -0,0 +1,16 @@
+import pytest
+
+# -----------------------------
+# Passing test
+# -----------------------------
+def test_pass_13():
+    assert 2 + 1 == 3
+
+def test_pass_23():
+    assert "allure" in "generate allure report"
+    
+def test_pass_33():
+    assert 2 + 10 == 12
+
+def test_pass_43():
+    assert 1 + 0 == 1
